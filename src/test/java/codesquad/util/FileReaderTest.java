6771cf8cbb39332c219ac355d32820b4e9da22ca
huiseung <whtjddn75@gmail.com>
2024-07-04 13:58:51 +0900

    [test] FileReaderTest 추가

---
+++ src/test/java/codesquad/util/FileReaderTest.java
@@ -0,0 +1,15 @@
+package codesquad.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FileReaderTest {
+    @Test
+    public void hasFile(){
+        assertTrue(FileReader.hasFile("/index.html"));
+        assertFalse(FileReader.hasFile("/inx.html"));
+    }
+
+}
