d87498a8a65b71c9a3217986400ce08b9018c380
huiseung <whtjddn75@gmail.com>
2024-07-08 16:57:34 +0900

    [fix] create GET 요청 제거

--- src/main/java/codesquad/application/handler/RegistrationHandler.java
+++ src/main/java/codesquad/application/handler/RegistrationHandler.java
@@ -1,6 +1,5 @@
 package codesquad.application.handler;
 
-import codesquad.application.domain.User;
 import codesquad.webserver.annotation.Handler;
 import codesquad.webserver.annotation.RequestMapping;
 import codesquad.webserver.http.HttpMethod;
@@ -14,19 +13,6 @@ import java.util.Map;
 @Handler
 public class RegistrationHandler {
     private final Logger log = LoggerFactory.getLogger(RegistrationHandler.class);
-
-    @RequestMapping(method = HttpMethod.GET, path = "/create")
-    public HttpResponse create(HttpRequest request){
-        String queryString = request.getQueryString();
-        String[] query = queryString.split("&");
-        String userId = query[0].split("=")[1];
-        String nickname = query[1].split("=")[1];
-        String password = query[2].split("=")[1];
-        User user = new User(userId, password, nickname);
-        log.debug("user: "+user);
-        return HttpResponse.createRedirectResponse();
-    }
-
     @RequestMapping(method = HttpMethod.POST, path="/create")
     public HttpResponse create2(HttpRequest request){
         Map<String, Object> bodyMessage = request.getHttpBody();
