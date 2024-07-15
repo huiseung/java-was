8f2f7bf8d2d3782115746ff7e3fbec453f8b8b69
huiseung <whtjddn75@gmail.com>
2024-07-15 16:39:21 +0900

    [feature] DataHandler 어노테이션, Specify 어토테이션, DataHandlerInitializer 클래스 추가 - DataHandler, Specify   - ApiHandler에 필드에 구현체를 생성자로 주입하기 위한 코드 - DataHandlerInitializer   - @DataHandler 가 붙은 클래스의 인스턴스 생성   - 어노티에션 value:인스턴스 맵을 반환

---
+++ src/main/java/codesquad/webserver/init/DataHandlerInitializer.java
@@ -0,0 +1,33 @@
+package codesquad.webserver.init;
+
+import codesquad.webserver.annotation.DataHandler;
+import codesquad.webserver.handler.DynamicResourceHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataHandlerInitializer {
+    private static final Logger log = LoggerFactory.getLogger(DataHandlerInitializer.class);
+
+    public static Map<String, Object> init(List<Class<?>> classes){
+        Map<String, Object> dataHandlerInstances = new HashMap<>();
+        for(Class<?> clazz : classes){
+            if(clazz.isAnnotationPresent(DataHandler.class)){
+                try{
+                    DataHandler dataHandler = clazz.getAnnotation(DataHandler.class);
+                    Object dataHandlerInstance = clazz.getDeclaredConstructor().newInstance();
+                    dataHandlerInstances.put(dataHandler.value(), dataHandlerInstance);
+                    return dataHandlerInstances;
+                } catch (Exception e){
+                    log.error(e.getMessage(), e);
+                }
+            }
+        }
+        return null;
+    }
+
+}
