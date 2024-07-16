f899e437f8d278eb1e2c7a32d0d7814c627ce0c9
huiseung <whtjddn75@gmail.com>
2024-07-16 16:48:47 +0900

    [refactoring] get all 시 생성 순 으로 반환

--- src/main/java/codesquad/application/datahandler/ArticleDataHandlerInMemory.java
+++ src/main/java/codesquad/application/datahandler/ArticleDataHandlerInMemory.java
@@ -3,25 +3,27 @@ package codesquad.application.datahandler;
 import codesquad.application.domain.Article;
 import codesquad.webserver.annotation.DataHandler;
 
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @DataHandler("ArticleDataHandlerInMemory")
-public class ArticleDataHandlerInMemory implements ArticleDataHandler{
+public class ArticleDataHandlerInMemory implements ArticleDataHandler {
     private Map<String, Article> store = new ConcurrentHashMap<>();
 
     @Override
     public void insert(Article article) {
         String key = UUID.randomUUID().toString();
-        article.setId(key);
-        store.put(key, article);
+        Article insertArticle = new Article(key, article.getTitle(), article.getContent(), article.getAuthor(), LocalDateTime.now());
+        store.put(key, insertArticle);
     }
 
     @Override
-    public List<Article> getAll(){
-        return store.values().stream().toList();
+    public List<Article> getAll() {
+        return store.values().stream().sorted(Comparator.comparing(Article::getCreatedDt)).toList();
     }
 
     @Override
