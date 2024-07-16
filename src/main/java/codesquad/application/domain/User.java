9d19975bc2165290240d6598a47dd9a400fe086d
huiseung <whtjddn75@gmail.com>
2024-07-16 11:27:07 +0900

    [feature] User에 id field 추가

--- src/main/java/codesquad/application/domain/User.java
+++ src/main/java/codesquad/application/domain/User.java
@@ -1,13 +1,22 @@
 package codesquad.application.domain;
 
-import java.util.Objects;
+
 
 public class User {
+    private final String id;
     private final String username;
     private final String password;
     private final String nickname;
 
+    public User(String id, String username, String password, String nickname) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
     public User(String username, String password, String nickname) {
+        this.id = null;
         this.username = username;
         this.password = password;
         this.nickname = nickname;
@@ -17,33 +26,19 @@ public class User {
         return this.password.equals(password);
     }
 
-    public String getNickname(){
-        return nickname;
+    public String getId() {
+        return id;
     }
 
-    public String getUserName(){
+    public String getUsername() {
         return username;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "username=" + username + '\'' +
-                ", password=" + password + '\'' +
-                ", nickname=" + nickname + '\'' +
-                '}';
+    public String getPassword() {
+        return password;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(nickname, user.nickname);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(username, password, nickname);
+    public String getNickname() {
+        return nickname;
     }
 }
