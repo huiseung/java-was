ae26835be3a4c5ec8c3aef5719bd14ff6fe0fc83
huiseung <whtjddn75@gmail.com>
2024-07-16 21:59:52 +0900

    [feature] multipart form data 처리

--- src/main/java/codesquad/webserver/http/HttpRequestParser.java
+++ src/main/java/codesquad/webserver/http/HttpRequestParser.java
@@ -1,11 +1,14 @@
 package codesquad.webserver.http;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,11 +18,21 @@ public class HttpRequestParser {
     public static HttpRequest parse(InputStream inputStream) {
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
         HttpRequestStartLine startLine = new HttpRequestStartLine(readStartLine(br));
+        log.info("[parse] start line "+startLine);
         HttpHeader header = readHeader(br);
+        log.info("[parse] header "+header);
         HttpBody body = null;
+
         if(header.hasKey("Content-Type") && header.hasKey("Content-Length")){
-            body = readBody(br, header.getValue("Content-Type"), Integer.parseInt(header.getValue("Content-Length")));
+            String contentType = header.getValue("Content-Type");
+            String contentLength = header.getValue("Content-Length");
+            if(contentType.startsWith("application/x-www-form-urlencoded")){
+                body = readUrlEncodedForm(br, Integer.parseInt(contentLength));
+            } else if(contentType.startsWith("multipart/form-data")){
+                body = readMultipartFormData(br, contentType);
+            }
         }
+        log.info("[parse] body "+body);
         return new HttpRequest(startLine, header, body);
     }
 
@@ -51,22 +64,87 @@ public class HttpRequestParser {
         }
     }
 
-    private static HttpBody readBody(BufferedReader br, String contentType, int contentLength){
+    private static HttpBody readUrlEncodedForm(BufferedReader br, int contentLength){
         try{
             char[] buffer = new char[contentLength];
             br.read(buffer, 0 , contentLength);
             String mesage = new String(buffer);
-            if(contentType.equals("application/json")){
-                return new HttpBody(HttpRequestBodyParser.parseJson(mesage));
-            }
-            else if(contentType.equals("application/x-www-form-urlencoded")){
-                return new HttpBody(HttpRequestBodyParser.parseForm(mesage));
-            }
-            else {
-                throw new IOException("");
-            }
+            return new HttpBody(HttpRequestBodyParser.parseForm(mesage));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static HttpBody readMultipartFormData(BufferedReader br, String contentType){
+        try{
+            Map<String, Object> jsonMap = new HashMap<>();
+            String boundary = contentType.substring(contentType.indexOf("boundary=") + "boundary=".length());
+            log.debug("[readMultipartFormData] boundary: "+ boundary);
+            String line;
+            while ((line = br.readLine()) != null) {
+                log.debug("[readMultipartFormData] line: "+line);
+                if (line.startsWith(boundary)) {
+                    processFormPart(br, jsonMap, boundary);
+                }
+                if (line.equals(boundary + "--")) {
+                    break;
+                }
+            }
+            return new HttpBody(jsonMap);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void processFormPart(BufferedReader reader, Map<String, Object> formData, String boundary) throws IOException {
+        String line = reader.readLine(); // Content-Disposition line
+        String[] contentDisposition = line.split(";");
+        String name = getFieldName(contentDisposition[1]);
+
+        if (line.contains("filename=")) {
+            String filename = getFileName(contentDisposition[2]);
+            reader.readLine(); // Content-Type line
+            reader.readLine(); // Empty line
+            ByteArrayOutputStream fileContent = readBinaryFileData(reader, boundary);
+            formData.put(name, new FileData(filename, fileContent.toByteArray()));
+        } else {
+            reader.readLine(); // Empty line
+            StringBuilder value = new StringBuilder();
+            while ((line = reader.readLine()) != null && !line.startsWith(boundary)) {
+                value.append(line);
+            }
+            formData.put(name, value.toString());
+        }
+    }
+
+    private static ByteArrayOutputStream readBinaryFileData(BufferedReader reader, String boundary)
+            throws IOException {
+        ByteArrayOutputStream fileContent = new ByteArrayOutputStream();
+        int ch;
+        while ((ch = reader.read()) != -1) {
+            if (ch == '\r') {
+                reader.mark(1);
+                if (reader.read() == '\n') {
+                    String nextLine = reader.readLine();
+                    if (nextLine.startsWith(boundary)) {
+                        break;
+                    }
+                }
+                reader.reset();
+            }
+            fileContent.write(ch);
+        }
+        return fileContent;
+    }
+
+    private static String getFieldName(String contentDisposition) {
+        return contentDisposition.substring(contentDisposition.indexOf("name=") + "name=".length()).replaceAll("\"", "").trim();
+    }
+
+    private static String getFileName(String contentDisposition) {
+        return contentDisposition.substring(contentDisposition.indexOf("filename=") + "filename=".length()).replaceAll("\"", "").trim();
+    }
+
+    record FileData(String filename, byte[] content) {
     }
 }
