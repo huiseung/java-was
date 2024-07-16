8179cc5ec616b7c4ceca82d7f7a43d813d27a09d
huiseung <whtjddn75@gmail.com>
2024-07-16 11:27:54 +0900

    [fix] 모든 @DataHandler 가 붙은 클래스들을 인스턴스 생성해 Map 에 담기

--- src/main/java/codesquad/webserver/init/DataHandlerInitializer.java
+++ src/main/java/codesquad/webserver/init/DataHandlerInitializer.java
@@ -1,11 +1,9 @@
 package codesquad.webserver.init;
 
 import codesquad.webserver.annotation.DataHandler;
-import codesquad.webserver.handler.DynamicResourceHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,13 +19,12 @@ public class DataHandlerInitializer {
                     DataHandler dataHandler = clazz.getAnnotation(DataHandler.class);
                     Object dataHandlerInstance = clazz.getDeclaredConstructor().newInstance();
                     dataHandlerInstances.put(dataHandler.value(), dataHandlerInstance);
-                    return dataHandlerInstances;
                 } catch (Exception e){
                     log.error(e.getMessage(), e);
                 }
             }
         }
-        return null;
+        return dataHandlerInstances;
     }
 
 }
