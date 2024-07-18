c4c96cfbe265911cded79d63f6a9f0ca867e96ba
huiseung <whtjddn75@gmail.com>
2024-07-18 21:43:15 +0900

    [test] user csv dat handler test

--- src/test/java/codesquad/webserver/database/csv/UserCsvDataTest.java
+++ src/test/java/codesquad/webserver/database/csv/UserCsvDataTest.java
@@ -1,6 +1,8 @@
 package codesquad.webserver.database.csv;
 
 import codesquad.application.domain.User;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +12,13 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CscDriverTest {
+public class UserCsvDataTest {
     private Logger log = LoggerFactory.getLogger(CsvJdbcDriver.class);
 
+
+
     @Test
-    public void test(){
+    public void userData(){
         try{
             Class.forName("codesquad.webserver.database.csv.CsvJdbcDriver");
 
@@ -33,6 +37,8 @@ public class CscDriverTest {
                     stmt.execute(query);
                     log.info("Users table created or already exists.");
                     insert();
+                    getAll();
+                    getByUsername();
                 }
             } catch (SQLException e) {
                 throw new RuntimeException(e);
@@ -57,4 +63,44 @@ public class CscDriverTest {
             log.error(e.getMessage(), e);
         }
     }
+
+    private void getAll(){
+        List<User> users = new ArrayList<>();
+        try(Connection con = CsvConnectionManager.getConnection(); Statement stmt = con.createStatement()){
+            String query = "SELECT * FROM USERS";
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()){
+                String id = rs.getString("ID");
+                String username = rs.getString("USERNAME");
+                String password = rs.getString("PASSWORD");
+                String nickname = rs.getString("NICKNAME");
+                log.info("id: {} username: {} password: {} nickname: {}", id, username, password, nickname);
+                users.add(new User(id, username, password, nickname));
+            }
+            assertEquals(1, users.size());
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private void getByUsername(){
+        String sql = "SELECT * FROM USERS WHERE USERS.USERNAME = ?";
+        String targetUsername = "a";
+        try(Connection con = CsvConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)){
+            pstmt.setString(1, targetUsername);
+            ResultSet rs = pstmt.executeQuery();
+            User user = null;
+            while (rs.next()) {
+                String id = rs.getString("ID");
+                String username = rs.getString("USERNAME");
+                String password = rs.getString("PASSWORD");
+                String nickname = rs.getString("NICKNAME");
+                user = new User(id, username, password, nickname);
+            }
+            assertEquals(targetUsername, user.getUsername());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
